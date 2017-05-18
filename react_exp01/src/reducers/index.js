846edd8f3bc5e6405cf316a3fc879bf376c5ce72
wind634 <2911184332@qq.com>
2017-05-18 16:56:32 +0800

    项目构建

---
+++ react_exp01/src/reducers/index.js
@@ -0,0 +1,30 @@
+/**
+ * Created by wangjiang on 17/5/18.
+ */
+import { combineReducers } from 'redux';
+import { nav } from './nav';
+
+const testReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'CHANGE_TEXT':
+            return {
+                text: state.text=='Hello' ? 'world':'Hello'
+            }
+        case 'BUTTON_CLICK':
+            return {
+                text: 'Hello world'
+            }
+        default:
+            return initialState;
+    }
+}
+
+const rootReducer = combineReducers({
+    //routing: routerReducer,
+    /* your reducers */
+    testReducer, //导航相关
+    //search, //搜索相关
+    //books, //书籍相关
+});
+
+export default rootReducer;
