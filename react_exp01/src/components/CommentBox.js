17ddf9e1adbb4012139bad1ef1e995fb3d4c57d1
wind634 <2911184332@qq.com>
2017-05-18 12:30:12 +0800

    项目构建

---
+++ react_exp01/src/components/CommentBox.js
@@ -0,0 +1,17 @@
+/**
+ * Created by wangjiang on 17/5/18.
+ */
+
+import React, { Component } from 'react';
+
+class CommentBox extends Component {
+    render() {
+        return (
+            <div className="commentBox">
+                Hello, world! I am a CommentBox.
+            </div>
+        );
+    }
+}
+
+export default CommentBox;
