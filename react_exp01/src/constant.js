8c978a03535c8cf2037cf0c5c257f7cc1e7f4e4c
wind634 <2911184332@qq.com>
2017-05-18 16:58:30 +0800

    项目构建

--- react_exp01/src/constant.js
+++ react_exp01/src/constant.js
@@ -1,3 +1,14 @@
 /**
  * Created by wangjiang on 17/5/18.
  */
+
+
+//action
+export const changeTextAction = {
+    type:'CHANGE_TEXT'
+}
+
+export const buttonClickAction = {
+    type:'BUTTON_CLICK'
+}
+
